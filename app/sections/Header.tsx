cdbc51994ad93923329f6959c4909803ad2998dc
ahasrati <hasratiamirhossein@gmail.com>
2024-05-30 21:14:13 -0400

    Small fixes for responsiveness

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -3,8 +3,8 @@ import Socials from "./Socials";
 export default function Header() {
   return (
     <div className="flex flex-col gap-4 text-center">
-      <h1 className="font-semibold text-5xl text-neutral-900">Amir Hasrati</h1>
-      <p className="font-light text-neutral-800">CS @ University of Guelph</p>
+      <h1 className="font-semibold text-5xl sm:text-6xl text-neutral-900">Amir Hasrati</h1>
+      <p className="sm:text-xl sm:font-normal font-light text-neutral-800">CS @ University of Guelph</p>
       <Socials />
     </div>
   );
