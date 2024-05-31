f9341be634ab6d3dab58efdb4015c24be456d453
ahasrati <hasratiamirhossein@gmail.com>
2024-05-30 21:11:06 -0400

    Fixed mobile version issues

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,13 +16,13 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="w-screen flex">
-      <div className="flex w-1/6">
+      <div className="hidden sm:flex w-1/6">
         <Left />
       </div>
-      <div className="w-2/3 flex justify-center">
+      <div className="sm:w-2/3 flex justify-center">
         <Center />
       </div>
-      <div className="flex w-1/6">
+      <div className="hidden sm:flex w-1/6">
         <Right />
       </div>
     </div>
