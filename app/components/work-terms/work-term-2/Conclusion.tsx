37a4884c0a65ec5051ad8e972d415e61bcb570b5
Amir Hasrati <ahasrati@uoguelph.ca>
2025-09-15 00:18:36 -0400

    Updated

--- app/components/work-terms/work-term-2/Conclusion.tsx
+++ app/components/work-terms/work-term-2/Conclusion.tsx
@@ -3,27 +3,15 @@ export default function Conclusion() {
     <section id="conclusion" className="flex flex-col gap-8">
       <h2 className="font-semibold text-neutral-900 text-xl">CONCLUSION</h2>
       <p className="text-neutral-600">
-        Before starting at Descartes, I was anxious to find out how my first work term experience was going to be like.
-        What if I don't perform well? What if I'm not as skilled as my manager expected and can't do the work I'm
-        assigned? What if... All of the overthinking and pessimistic thoughts made me feel out of place.
-        <br />
-      </p>
-      <p className="text-neutral-600">
-        In some ways, I think this shows how much I cared about this opportunity. I didn't want to waste it so I put
-        everything I could into it. To my surprise, the stories I've heard from my friends about their negative
-        experiences with their internships and managers didn't apply to me at all. I, and the other co-ops were
-        immediately taken in as a part of the team, and throughout the semester, had the chance to work on and learn
-        about what interested us.
-        <br />
-      </p>
-      <p className="text-neutral-600">
-        I honed and enhanced my web development skills, learned the importance and applications of cyber security in web
-        applications, and worked on creating user-friendly and fluid interfaces. For a large portion of my work-term, I
-        worked on implementing data visualization controls for our team. I refreshed our Pie, Bar, and Line chart
-        controls and provided pages on the company demo application that demonstrate the implementation of these
-        controls. I also worked on porting an old version of one of our applications to match our latest standards,
-        including user-experience, styling, and moving over the app from .NET Framework 4.8 to .NET Core 8.0. Overall,
-        it was the ideal work term and I can't wait to be back at Descartes in the summer!
+        During my second work term at Descartes, I had the opportunity to take on more responsibility and contribute to
+        a company-wide application used across the enterprise. I developed core features for the SSO Validation
+        platform, which strengthened my understanding of security compliance in web applications, APIs, and identity
+        providers. Beyond backend development, I worked closely with the UX department to design and refine UI
+        components, improving user interaction flows and making the application more intuitive for developers across
+        teams. This blend of technical development and cross-functional collaboration not only expanded my technical
+        skillset in .NET Core, but also gave me valuable experience in creating software that balances functionality
+        with usability. Overall, this term reinforced my passion for building impactful tools and showed me the
+        importance of collaboration in delivering high-quality solutions.
       </p>
     </section>
   );
